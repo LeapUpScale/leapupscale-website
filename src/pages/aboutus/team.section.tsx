adb66152ab57c7e03bc419fe999b371ca663453f
Joe <jahmad92@hotmail.com>
2025-10-09 05:14:54 +0200

    About Us / Company page

---
+++ src/pages/aboutus/team.section.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+
+const people = [
+  {
+    name: "Meghan Castellano",
+    role: "Co-Founder / CEO",
+    imageUrl: "/Valkyrie.png",
+  },
+  {
+    name: "Meghan Castellano",
+    role: "Business Relations",
+    imageUrl: "/Valkyrie.png",
+  },
+  {
+    name: "Meghan Castellano",
+    role: "The Contractor",
+    imageUrl: "/Valkyrie.png",
+  },
+  // {
+  //   name: "Psyzee",
+  //   role: "Designer",
+  //   imageUrl: "/financial-advisor.png",
+  // },
+  {
+    name: "Meghan Castellano",
+    role: "IT Tech Support",
+    imageUrl: "/Valkyrie.png",
+  },
+];
+
+export default function TeamSection() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto grid max-w-7xl gap-20 px-6 lg:px-8 xl:grid-cols-3">
+        <div className="max-w-xl">
+          <h2 className="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">
+            Meet our leadership
+          </h2>
+          <p className="mt-6 text-lg/8 text-gray-600">
+            We’re a dynamic group of individuals who are passionate about what
+            we do and dedicated to delivering the best results for our clients.
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
+        >
+          {people.map((person) => (
+            <li key={person.name}>
+              <div className="flex items-center gap-x-6">
+                <Image
+                  alt=""
+                  src={person.imageUrl}
+                  className="size-16 rounded-full outline-1 -outline-offset-1 outline-black/5"
+                  width={64}
+                  height={64}
+                />
+                <div>
+                  <h3 className="text-base/7 font-semibold tracking-tight text-gray-900">
+                    {person.name}
+                  </h3>
+                  <p className="text-sm/6 font-semibold text-indigo-600">
+                    {person.role}
+                  </p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
